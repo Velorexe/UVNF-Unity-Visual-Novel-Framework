058cbc425d34404b92784c65de86ac3137e884fe
Velorexe <david.bruschke@gmail.com>
2020-04-09 10:49:09 +0200

    Started scripts for Saving

---
+++ Project/Assets/Scripts/Core/Saving/Save.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class Save
+{
+    public SaveLocationTypes LocationType;
+
+    public string SavePath
+    {
+        get { return _savePath; }
+        set
+        {
+            if (Directory.Exists(value))
+                _savePath = value;
+        }
+    }
+    private string _savePath;
+
+    public Save(SaveData saveData)
+    {
+        switch (LocationType)
+        {
+            case SaveLocationTypes.Appdata:
+                SavePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), Application.productName, "Saves");
+                break;
+            case SaveLocationTypes.Documents:
+                SavePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), Application.productName, "Saves");
+                break;
+            case SaveLocationTypes.GameLocation:
+                SavePath = Path.Combine(Application.dataPath, "Saves");
+                break;
+        }
+    }
+}
