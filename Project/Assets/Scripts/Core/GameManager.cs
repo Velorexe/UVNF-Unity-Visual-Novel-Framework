887f01e2c02a09eea7be754e78173192f88311f9
Velorexe <david.bruschke@gmail.com>
2020-04-09 10:19:52 +0200

    Created basic UI and UI sprites

---
+++ Project/Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core;
+using Entities;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Manager;
+
+    public static Player Player;
+    public static Location CurrentLocation;
+
+    private Dictionary<string, object[]> _eventFlags;
+
+    public bool AddEventFlag(string eventFlag, params object[] eventValues)
+    {
+        if (_eventFlags.ContainsKey(eventFlag))
+            return false;
+        _eventFlags.Add(eventFlag, eventValues);
+        return true;
+    }
+    
+    public bool ReachedEventFlag(string eventFlag)
+    {
+        return _eventFlags.ContainsKey(eventFlag);
+    }
+
+    public object[] GetEventFlagValues(string eventFlag)
+    {
+        if (!_eventFlags.ContainsKey(eventFlag))
+            return null;
+        return _eventFlags[eventFlag];
+    }
+}
---
+++ Project/Assets/Scripts/Entities/Character/Person.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Entities
+{
+    [Serializable]
+    public abstract class Person
+    {
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        private string _firstName = "Velo";
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        private string _lastName = "Rex";
+
+        public string NickName
+        {
+            get { return _nickName; }
+            set { _nickName = value; }
+        }
+        private string _nickName = "Rexxy";
+    }
+}
