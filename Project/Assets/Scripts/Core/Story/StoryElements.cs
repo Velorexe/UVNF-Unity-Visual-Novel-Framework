5c7c6fb3896758a1d6e804282dfd54f2fd684766
Velorexe <david.bruschke@gmail.com>
2020-04-10 19:06:06 +0200

    Added overal UDSFCanvas script for handling front end components

--- Project/Assets/Scripts/Core/Story/StoryElements.cs
+++ Project/Assets/Scripts/Core/Story/StoryElements.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEditor;
 using Entities;
 
 public class StoryElements
 {
+    [Serializable]
     public class DialogueElement : StoryElement
     {
         public override string ElementName { get { return "Dialogue"; } }
@@ -22,9 +24,29 @@ public class StoryElements
             Dialogue = EditorGUILayout.TextArea(Dialogue, GUILayout.ExpandHeight(true), GUILayout.MaxHeight(50));
         }
 
-        public override void Execute()
+        public override IEnumerable Execute()
         {
+            return null;
+        }
+    }
 
+    [Serializable]
+    public class SpawnObjectElement : StoryElement
+    {
+        public override string ElementName { get { return "Spawn Object"; } }
+        public override Color32 DisplayColor { get { return new Color32(0x99, 0xbd, 0xf7, 0xff); } }
+
+        public GameObject ObjectToSpawn;
+
+        public override void DisplayLayout()
+        {
+            GUILayout.Label("Object To Spawn");
+            ObjectToSpawn = EditorGUILayout.ObjectField(ObjectToSpawn, typeof(GameObject), false) as GameObject;
+        }
+
+        public override IEnumerable Execute()
+        {
+            return null;
         }
     }
 }
---
+++ Project/Assets/Scripts/Core/UI/TextDisplayStyle.cs
@@ -0,0 +1,10 @@
+﻿public enum TextDisplayStyle
+{
+    Small,
+    Big,
+    Italic,
+    Bold,
+    Fast,
+    Slow,
+    Gigantic
+}
