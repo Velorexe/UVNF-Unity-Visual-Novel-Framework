138c581b2067c52e43c3386300d0452db9d3cc57
Velorexe <david.bruschke@gmail.com>
2020-04-17 13:39:27 +0200

    Started working on Characters on screen

---
+++ Project/Assets/UVNF/Scripts/Core/CanvasCharacterManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasCharacterManager : MonoBehaviour
+{
+    public List<Transform> CharactersOnScreen;
+
+    public Transform LeftSideCharacterStack;
+    public Transform MiddleCharacterStack;
+    public Transform RightSideCharacterStack;
+
+    public void AddCharacter(Sprite characterSprite, ScenePositions enter, ScenePositions position)
+    {
+
+    }
+}
